e65a34087eb93286a4bbc14c262b0783b87ab5d8
LukasMandik <116304216+LukasMandik@users.noreply.github.com>
2023-02-20 20:19:39 +0100

    Delete me_asset.py

--- data/asset/me_asset.py
+++
@@ -1,9 +0,0 @@
-SPOTIPY_CLIENT_ID ='9f026abb62ad4c63a952c87640fbe3ee'
-SPOTIPY_CLIENT_SECRET ='732f3849425a413e9d04f1f9c6e07355'
-SPOTIPY_REDIRECT_URI ='https://open.spotify.com/playlist/65Gg8X7NVU3g95Az5aXHgo'
-SPOTIFY_SCOPES = [
-    'playlist-modify-public',
-    'user-library-read',
-    'user-library-modify',
-]
-SPOTIPY_PLAYLIST_ID = '65Gg8X7NVU3g95Az5aXHgo'
